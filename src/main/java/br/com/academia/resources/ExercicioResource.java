a23dc876b2c9601714a9885aa69c7e0f0c65ab40
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-03-23 12:48:28 -0300

    Trocando List<> para Page<> em exercicios

--- src/main/java/br/com/academia/resources/ExercicioResource.java
+++ src/main/java/br/com/academia/resources/ExercicioResource.java
@@ -1,18 +1,18 @@
 package br.com.academia.resources;
 
 import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -29,10 +29,14 @@ public class ExercicioResource {
 	private ExercicioService exercicioService;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<ExercicioDTO>> list()
+	public ResponseEntity<Page<ExercicioDTO>> list(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC")String direction)
 	{
-		List<Exercicio> list = exercicioService.list();
-		List<ExercicioDTO> listDto = list.stream().map(exercicio -> new ExercicioDTO(exercicio)).collect(Collectors.toList());
+		Page<Exercicio> list = exercicioService.findPage(page, linesPerPage, orderBy, direction);
+		Page<ExercicioDTO> listDto = list.map(exercicio -> new ExercicioDTO(exercicio));
 		return ResponseEntity.ok().body(listDto);
 	}
 	
--- src/main/java/br/com/academia/services/ExercicioService.java
+++ src/main/java/br/com/academia/services/ExercicioService.java
@@ -3,6 +3,9 @@ package br.com.academia.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.academia.domain.Exercicio;
@@ -87,5 +90,10 @@ public class ExercicioService {
 		}
 		
 		return exercicio;
+	}
+
+	public Page<Exercicio> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page,  linesPerPage, Direction.valueOf(direction), orderBy);
+		return exercicioRepository.findAll(pageRequest);
 	}
 }
