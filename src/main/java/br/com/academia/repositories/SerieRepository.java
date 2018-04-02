9d3030930ee5b8a9817962c1e49421fcda703785
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-04-01 22:16:24 -0300

    Ajustando o like para case sensitive

--- src/main/java/br/com/academia/repositories/SerieRepository.java
+++ src/main/java/br/com/academia/repositories/SerieRepository.java
@@ -17,7 +17,7 @@ public interface SerieRepository extends JpaRepository<Serie, Integer>{
 	@Query("Select sr from Serie sr INNER JOIN sr.solicitacao sol INNER JOIN sol.usuario WHERE sol.usuario.id = :usuario_logado")
 	Page<Serie> findByUserLogged(Pageable pageRequest, @Param("usuario_logado") Integer id);
 
-	@Query("Select sr from Serie sr INNER JOIN sr.solicitacao sol WHERE sol.solicitante LIKE %:nome%")
+	@Query("Select sr from Serie sr INNER JOIN sr.solicitacao sol WHERE lower(sol.solicitante) LIKE lower(concat('%', :nome ,'%'))")
 	List<Serie> findBySolicitante(@Param("nome") String nome);
 
 }
